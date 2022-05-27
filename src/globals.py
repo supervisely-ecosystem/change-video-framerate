9e9ec62c8fe567a24073192c8c74176362b407c0
alexander fomin <af.tmpgooacc@gmail.com>
2022-05-27 13:23:02 +0300

    comment debug lines

--- src/globals.py
+++ src/globals.py
@@ -14,14 +14,14 @@ sys.path.append(app_root_directory)
 data_directory = '/var/sly-app'   # @TODO: ok?
 
 # debug code BEGIN
-from dotenv import load_dotenv
-
-debug_env_path = os.path.join(app_root_directory, "debug.env")
-secret_debug_env_path = os.path.join(app_root_directory, "secret_debug.env")
-load_dotenv(debug_env_path)
-load_dotenv(secret_debug_env_path, override=True)
-sly.logger.setLevel(os.environ['LOG_LEVEL'].upper())    # because sly logger has been already initialized
-data_directory = os.environ['LOCAL_DEBUG_DATA_DIR']
+# from dotenv import load_dotenv
+#
+# debug_env_path = os.path.join(app_root_directory, "debug.env")
+# secret_debug_env_path = os.path.join(app_root_directory, "secret_debug.env")
+# load_dotenv(debug_env_path)
+# load_dotenv(secret_debug_env_path, override=True)
+# sly.logger.setLevel(os.environ['LOG_LEVEL'].upper())    # because sly logger has been already initialized
+# data_directory = os.environ['LOCAL_DEBUG_DATA_DIR']
 # debug code END
 
 
